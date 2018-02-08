44a891d56ac9c3d7632706ecaf6466de0be041f4
Arash <arash.giani@gmail.com>
2018-02-08 09:34:08 -0800

    toggle

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 
 
 <div class="container">
-<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+<nav class="navbar navbar-light bg-faded">
   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
