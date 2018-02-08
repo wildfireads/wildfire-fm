3b94146dd06d57e42a89d72c0f3ba6f53174ae04
Arash <arash.giani@gmail.com>
2018-02-08 09:41:48 -0800

    color

--- index.php
+++ index.php
@@ -29,8 +29,8 @@
 
 
 
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow" style="background-color: transparent;">
-      <h5 class="my-0 mr-md-auto font-weight-normal">Company name</h5>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow" style="background-color: rgb(110, 85, 52);">
+      <h5 class="my-0 mr-md-auto">Company name</h5>
       <nav class="my-2 my-md-0 mr-md-3">
         <a class="p-2 text-dark" href="#">Features</a>
         <a class="p-2 text-dark" href="#">Enterprise</a>
