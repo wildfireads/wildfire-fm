6533c58b78a1d9d91b822b0f2fcf3ad5b3556a36
Arash <arash.giani@gmail.com>
2018-02-08 09:45:31 -0800

    padding

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 
 
 
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3" style="background-color: rgb(110, 85, 52); border:none; margin:0;">
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3" style="background-color: rgb(110, 85, 52); border:none; margin:0; padding: 0">
       <h5 class="my-0 mr-md-auto">Company name</h5>
       <nav class="my-2 my-md-0 mr-md-3">
         <a class="p-2 text-dark" href="#">Features</a>
