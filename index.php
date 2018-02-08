ab549045c44c6ce5d37a2fb22e08dd30f995eb4c
Arash <arash.giani@gmail.com>
2018-02-08 09:39:01 -0800

    mod

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
   <div class="container" style="background-color: rgb(110, 85, 52); border:5px solid rgb(110, 85, 52);">
 
 
-<div class="container">
+
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
       <h5 class="my-0 mr-md-auto font-weight-normal">Company name</h5>
       <nav class="my-2 my-md-0 mr-md-3">
@@ -39,7 +39,7 @@
       </nav>
       
     </div>
-</div>
+
 
 
 </div>
