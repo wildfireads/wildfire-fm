460bd1d81dec136ea2d0f4182a4359770b9da2dc
Arash <arashgiani84@gmail.com>
2018-03-01 18:20:14 -0500

    Pico

--- index.php
+++ index.php
@@ -1,17 +1,26 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<?php // @codingStandardsIgnoreFile
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+// check PHP platform requirements
+if (PHP_VERSION_ID < 50306) {
+    die('Pico requires PHP 5.3.6 or above to run');
+}
+if (!extension_loaded('dom')) {
+    die('Pico requires the PHP extension "dom" to run');
+}
+if (!extension_loaded('mbstring')) {
+    die('Pico requires the PHP extension "mbstring" to run');
+}
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+// load dependencies
+require_once(__DIR__ . '/vendor/autoload.php');
+
+// instance Pico
+$pico = new Pico(
+    __DIR__,    // root dir
+    'config/',  // config dir
+    'plugins/', // plugins dir
+    'themes/'   // themes dir
+);
+
+// run application
+echo $pico->run();
